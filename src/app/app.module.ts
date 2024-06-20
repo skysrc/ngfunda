8d15e88884e6f80b13093c8400b5872192db6332
sky <skydeutsch@yahoo.com>
2024-06-21 00:19:06 +0800

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/expense/expense-details/expense-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: './event-details.component.html',
+    styles: [`
+    .container {padding-left:20px; padding-right: 20px;}
+    .event-image { height: 10px;}
+    `]
+})
+export class ExpenseDetailsComponent {
+
+}
