9b07f2e5acbbc48824a413c0f4292f1e1f06d813
sky <skydeutsch@yahoo.com>
2024-09-11 00:43:36 +0800

    style component

--- src/app/expense/expense-thumbnail.ts
+++ src/app/expense/expense-thumbnail.ts
@@ -6,9 +6,18 @@ import { Component, EventEmitter, input, Input, Output, output } from "@angular/
     <div class="well hoverwell thumbnail">
         <h2>{{expense.name}}</h2>
         <div> Date : {{expense.date}} </div>
+        <div>
+            <span class="pad-left">
+                {{expense.time}}
+            </span>
+        </div>
     </div>
-    
-    `
+    `,
+    styles: [`
+            .pad-left { margin-left: 10px;}
+            .well div {color:#bbb}
+        `
+    ]
 })
 export class ExpenseThumbnailComponent {
     @Input() expense: any
